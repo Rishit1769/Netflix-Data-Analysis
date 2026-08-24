ce9bab3c26a77f2c15aaebad24c003bdc104a1b6
Rishit Singh <rishitsingh.286@gmail.com>
2026-08-25 00:50:17 +0530

    Enhance data enrichment by stripping whitespace from 'date_added' and extracting 'year_added'

--- Data-Enrichment.py
+++ Data-Enrichment.py
@@ -2,4 +2,8 @@ import pandas as pd
 
 df = pd.read_csv("netflix_titles.csv")
 
-df["date_added"] = pd.to_datetime(df["date_added"])
+df["date_added"] = pd.to_datetime(df["date_added"].str.strip())
+
+df["year_added"] = df["date_added"].dt.year
+
+print(df["year_added"])
